332312894d85390cd5dc442f9292a0777dab33bd
harri-renney <Renney77@gmail.com>
2019-11-19 01:15:27 +0000

    Commit before ADC.

--- mindControlSynth.py
+++ mindControlSynth.py
@@ -13,7 +13,16 @@ import RPi.GPIO as gpio
 from pinaps.piNapsController import PiNapsController
 from pinaps.blinoParser import BlinoParser
 
+#Second-order differential equation of motion#
+def positionStep(pos, vel, acc):
+    return pos + vel * 2 + (1/2) * acc * 4
+
+def velocityStep(vel, acc):
+    return acc * 2 + vel
+
 CTRL_LFO_PITCH = 26
+CTRL_LFO_RATE = 29
+
 MIDI_MESSAGE_PERIOD = 1
 
 clock = pygame.time.Clock()
@@ -21,30 +30,42 @@ clock = pygame.time.Clock()
 print(mido.get_output_names())
 
 port = mido.open_output('USB Midi:USB Midi MIDI 1 20:0')
+msgModulate = mido.Message('control_change', control=CTRL_LFO_PITCH, value=100)
+port.send(msgModulate)
 
 ##Pinaps Setup##
 pinapsController = PiNapsController()
 pinapsController.defaultInitialise()
 blinoParser = BlinoParser()
 
-vibratoStrength = 0
+VibratoPos = 0
+vibratoVel = 0
+vibratoAcc = 4  #Replace with acceleration modifier to effect the EEG attention value.
 while True:
-        while(pinapsController.dataWaiting()):
-            data = pinapsController.readEEGSensor()
-            blinoParser.parseByte(data)
+    while(pinapsController.dataWaiting()):
+        data = pinapsController.readEEGSensor()
+        blinoParser.parseByte(data)
 
-        ##Change in vibratoStrength depending on meditation values##
-        ##@ToDo - Change to include more momentum build up etc##
-        if(blinoParser.meditation < 50):
-            vibratoStrength -= 5
-            vibratoStrength = 0 if vibratoStrength < 0 else vibratoStrength
-        else:
-            vibratoStrength += 5
-            vibratoStrength = 100 if vibratoStrength > 100 else vibratoStrength
+    ##Change in vibratoStrength depending on meditation values##
+    ##@ToDo - Change to include more momentum build up etc##
+    if(blinoParser.attention > 50):
+        VibratoPos = positionStep(VibratoPos, vibratoVel, vibratoAcc)
+        vibratoVel = velocityStep(vibratoVel, vibratoAcc)
+        VibratoPos = 100 if VibratoPos > 100 else VibratoPos
+        VibratoPos = 0 if VibratoPos < 0 else VibratoPos
+    else:
+        VibratoPos = positionStep(VibratoPos, vibratoVel, -vibratoAcc)
+        vibratoVel = velocityStep(vibratoVel, -vibratoAcc)
+        VibratoPos = 100 if VibratoPos > 100 else VibratoPos
+        VibratoPos = 0 if VibratoPos < 0 else VibratoPos
 
-        msgModulate = mido.Message('control_change', control=CTRL_LFO_PITCH, value=vibratoStrength)
-        port.send(msgModulate)
+    print("Signal Quality : ", blinoParser.quality)
+    print("Attention level : ", blinoParser.attention)
+    print("Message vibrato strength: ", VibratoPos)
 
-        #sleep or tick?#
-        time.sleep(MIDI_MESSAGE_PERIOD)
-        #clock.tick(MIDI_MESSAGE_PERIOD)
+    msgModulate = mido.Message('control_change', control=CTRL_LFO_RATE, value=VibratoPos)
+    port.send(msgModulate)
+
+    #sleep or tick?#
+    time.sleep(MIDI_MESSAGE_PERIOD)
+    #clock.tick(MIDI_MESSAGE_PERIOD)
