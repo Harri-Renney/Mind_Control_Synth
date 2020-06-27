b2a5740d4717a8ea3104f2a047bf8159a9d86995
harri-renney <Renney77@gmail.com>
2020-06-27 13:05:08 +0100

    Updated to use new NeuroParser library released.

--- mindControlSynth.py
+++ mindControlSynth.py
@@ -11,7 +11,7 @@ import mido
 import RPi.GPIO as gpio
 
 from pinaps.piNapsController import PiNapsController
-from pinaps.blinoParser import BlinoParser
+from NeuroParser import NeuroParser
 
 #Second-order differential equation of motion#
 def positionStep(pos, vel, acc):
@@ -25,47 +25,63 @@ CTRL_LFO_RATE = 29
 
 MIDI_MESSAGE_PERIOD = 1
 
-clock = pygame.time.Clock()
-
-print(mido.get_output_names())
-
-port = mido.open_output('USB Midi:USB Midi MIDI 1 20:0')
-msgModulate = mido.Message('control_change', control=CTRL_LFO_PITCH, value=100)
-port.send(msgModulate)
-
-##Pinaps Setup##
-pinapsController = PiNapsController()
-pinapsController.defaultInitialise()
-blinoParser = BlinoParser()
-
 VibratoPos = 0
 vibratoVel = 0
 vibratoAcc = 4  #Replace with acceleration modifier to effect the EEG attention value.
-while True:
-    while(pinapsController.dataWaiting()):
-        data = pinapsController.readEEGSensor()
-        blinoParser.parseByte(data)
+def printCallback(packet):
+    global VibratoPos
+    global vibratoVel
+    global vibratoAcc
+    if(packet.code == NeuroParser.DataPacket.kPoorQuality):
+        print("Poor quality: " + str(packet.poorQuality))
+    if(packet.code == NeuroParser.DataPacket.kAttention):
+        print("Attention: " + str(packet.attention))
 
-    ##Change in vibratoStrength depending on meditation values##
-    ##@ToDo - Change to include more momentum build up etc##
-    if(blinoParser.attention > 50):
-        VibratoPos = positionStep(VibratoPos, vibratoVel, vibratoAcc)
-        vibratoVel = velocityStep(vibratoVel, vibratoAcc)
-        VibratoPos = 100 if VibratoPos > 100 else VibratoPos
-        VibratoPos = 0 if VibratoPos < 0 else VibratoPos
-    else:
-        VibratoPos = positionStep(VibratoPos, vibratoVel, -vibratoAcc)
-        vibratoVel = velocityStep(vibratoVel, -vibratoAcc)
-        VibratoPos = 100 if VibratoPos > 100 else VibratoPos
-        VibratoPos = 0 if VibratoPos < 0 else VibratoPos
+        ##Change in vibratoStrength depending on meditation values##
+        ##@ToDo - Change to include more momentum build up etc##
+        if(packet.attention > 50):
+            VibratoPos = positionStep(VibratoPos, vibratoVel, vibratoAcc)
+            vibratoVel = velocityStep(vibratoVel, vibratoAcc)
+            VibratoPos = 100 if VibratoPos > 100 else VibratoPos
+            VibratoPos = 0 if VibratoPos < 0 else VibratoPos
+        else:
+            VibratoPos = positionStep(VibratoPos, vibratoVel, -vibratoAcc)
+            vibratoVel = velocityStep(vibratoVel, -vibratoAcc)
+            VibratoPos = 100 if VibratoPos > 100 else VibratoPos
+            VibratoPos = 0 if VibratoPos < 0 else VibratoPos
+        
 
-    print("Signal Quality : ", blinoParser.quality)
-    print("Attention level : ", blinoParser.attention)
-    print("Message vibrato strength: ", VibratoPos)
+pinapsController = PiNapsController()
 
-    msgModulate = mido.Message('control_change', control=CTRL_LFO_RATE, value=VibratoPos)
+def main():
+    #Init interface.
+    clock = pygame.time.Clock()
+
+    print(mido.get_output_names())
+
+    port = mido.open_output('USB Midi:USB Midi MIDI 1 20:0')
+    msgModulate = mido.Message('control_change', control=CTRL_LFO_PITCH, value=100)
     port.send(msgModulate)
 
-    #sleep or tick?#
-    time.sleep(MIDI_MESSAGE_PERIOD)
-    #clock.tick(MIDI_MESSAGE_PERIOD)
+    #Init Pinaps.
+    pinapsController.defaultInitialise()
+    pinapsController.setFullMode()
+
+    pinapsController.deactivateAllLEDs()
+
+    aParser = NeuroParser()
+
+    while True:
+        data = pinapsController.readEEGSensor()
+        aParser.parse(data, printCallback)
+
+        print("Message vibrato strength: ", VibratoPos)
+        msgModulate = mido.Message('control_change', control=CTRL_LFO_RATE, value=VibratoPos)
+        port.send(msgModulate)
+
+        #sleep or tick?#
+        time.sleep(MIDI_MESSAGE_PERIOD)
+        #clock.tick(MIDI_MESSAGE_PERIOD)
+
+if __name__ == '__main__':
+    main()
