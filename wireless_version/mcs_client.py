12d65c9050e74ef21be0c8e0ee1131019c41ba6b
harri-renney <Renney77@gmail.com>
2020-07-03 17:40:30 +0100

    Client side updated.

--- wireless_version/mcs_client.py
+++ wireless_version/mcs_client.py
@@ -1,5 +1,11 @@
+import sys
 import mido
-from bluepy.btle import Scanner, DefaultDelegate
+import bluetooth
+
+CTRL_LFO_PITCH = 26
+CTRL_LFO_RATE = 29
+
+MIDI_MESSAGE_PERIOD = 1
 
 def main():
     #Init interface.
@@ -9,19 +15,34 @@ def main():
     msgModulate = mido.Message('control_change', control=CTRL_LFO_PITCH, value=100)
     port.send(msgModulate)
 
-    scanner = Scanner()
-    devices = scanner.scan(10.0)
+    # search for the SampleServer service
+    addr = 'B8:27:EB:F6:3E:A6'
+    uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+    service_matches = bluetooth.find_service(uuid=uuid, address=addr)
 
-    for dev in devices:
-        print "Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi)
-        for (adtype, desc, value) in dev.getScanData():
-            print "  %s = %s" % (desc, value)
+    if len(service_matches) == 0:
+        print("Couldn't find the SampleServer service.")
+        sys.exit(0)
+
+    first_match = service_matches[0]
+    port = first_match["port"]
+    name = first_match["name"]
+    host = first_match["host"]
+
+    print("Connecting to \"{}\" on {}".format(name, host))
+
+    # Create the client socket
+    sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    sock.connect((host, port))
+
+    port = mido.open_output('USB Midi:USB Midi MIDI 1 20:0')
 
     while True:
-        #READ OVER BLUETOOTH VIBRATO.
-        VibratoPos = 0.0
-        print("Message vibrato strength: ", VibratoPos)
-        msgModulate = mido.Message('control_change', control=CTRL_LFO_RATE, value=VibratoPos)
+        vibratoStr = sock.recv(1024)
+        print("Received", vibratoStr)
+        vibratoData = int(vibratoStr)
+        msgModulate = mido.Message('control_change', control=CTRL_LFO_RATE, value=vibratoData)
+        print(msgModulate)
         port.send(msgModulate)
 
 
